5e3007aad5bdf727f89678f13578223858e07448
e-popovic <popovic.ema59@gmail.com>
2022-12-09 15:05:46 +0100

    end message

--- evaluation-app/image-eval/src/app/app.component.ts
+++ evaluation-app/image-eval/src/app/app.component.ts
@@ -15,6 +15,8 @@ export class AppComponent {
   valueB = 50;
   results = ['']
   startScreen = true;
+  message = false;
+  messageText = "Next set:";
 
   start() {
     this.startScreen = false;
@@ -28,8 +30,6 @@ export class AppComponent {
       this.shuffle(variants);
 
       for (const element of variants) {
-        this.evalAllowed = false;
-        this.valueA = this.valueB = 50;
         let imagesrcs = [i.toString(), i.toString()+element];
         this.shuffle(imagesrcs);
 
@@ -64,9 +64,21 @@ export class AppComponent {
 
         this.imgTitle = '-';
         this.imgSrc = 'assets/images/grey.png';
-        await this.delay(3000);   // 5000 (5-11 s)
+        await this.delay(2000);   // 5000 (5-11 s)
 
+        this.evalAllowed = false;
         this.results.push(imagesrcs[0] + '|' + imagesrcs[1] + ":" + (this.valueA - this.valueB).toString() + "\n");
+        this.valueA = this.valueB = 50;
+
+        if (i == 2 && element === variants[variants.length-1]) {  // i je zadnji, promjenjivo!
+          this.messageText = "Thank you for your time!";
+          this.message = true;
+        }
+        else {
+          this.message = true;
+          await this.delay(2000);
+          this.message = false;
+        }
       }
     }
     let blob = new Blob(this.results,{type:"text/plain;charset=utf-8"});
