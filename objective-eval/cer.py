8c209d0027c4e8424f54c954809225106202eee1
e-popovic <popovic.ema59@gmail.com>
2023-01-27 10:43:33 +0100

    objective eval code

--- objective-eval/cer.py
+++ objective-eval/cer.py
@@ -5,43 +5,34 @@ import numpy as np
 import math
 
 start_time = time.time()
-
 image_original = []
 names_original = []
 image_method = []
 names_method = []
-
 file_cer = open("cer_calculated.txt", "a")
-
 for filename in os.scandir('images/originals'): 
     if filename.is_file():
         im1=cv2.imread(filename.path)
         image_original.append(im1)
         names_original.append(filename.path)
-
 for filename in os.scandir('images/edited'): 
     if filename.is_file():
         im2=cv2.imread(filename.path)
         image_method.append(im2)
         names_method.append(filename.path)
-
 count=0
+
 while(count<48):
     count_original = math.floor(count/4)
-
     if (count/4 == math.floor(count/4)):
         yuv_img1 = cv2.cvtColor(image_original[count_original], cv2.COLOR_BGR2YUV)
         y1, u1, v1 = cv2.split(yuv_img1) 
-
     yuv_img2 = cv2.cvtColor(image_method[count], cv2.COLOR_BGR2YUV)
     y2, u2, v2 = cv2.split(yuv_img2)
-
     img_name = names_method[count].split('\\')[1][:-4]
     print(img_name)
-
     file_cer.write(img_name)
     file_cer.write('/')
-
     u1_niz = np.array(u1).astype(np.float64)  
     v1_niz = np.array(v1).astype(np.float64)  
     u2_niz = np.array(u2).astype(np.float64)  
@@ -50,7 +41,6 @@ while(count<48):
     niz_v1 = v1_niz.ravel()
     niz_u2 = u2_niz.ravel()
     niz_v2 = v2_niz.ravel()
-
     counter=0
     vector_sum = 0
     suma1=0
@@ -69,12 +59,9 @@ while(count<48):
         vector_sum = vector_sum + vector
         vector_array.append(vector)
         counter = counter+1
-
     cer = 10 * math.log10((suma1/13488000)/(suma2/13488000))
-
     data = (str(vector_sum) + ':' + str(cer) + '\n' )
     file_cer.write(data)
-
     count = count+1
     cv2.waitKey(0)
 
--- objective-eval/psnr_ssim_mse.py
+++ objective-eval/psnr_ssim_mse.py
@@ -5,48 +5,40 @@ import os
 import math
 
 start_time = time.time()
-
 image_original = []
 names_original = []
 image_method = []
 names_method = []
-
 file_psnr = open("psnr_calculated.txt", "a")
 file_ssim = open("ssim_calculated.txt", "a")
 file_mse = open("mse_calculated.txt", "a")
-
 for filename in os.scandir('images/originals'): 
     if filename.is_file():
         im1=cv2.imread(filename.path)
         image_original.append(im1)
         names_original.append(filename.path)
-
 for filename in os.scandir('images/edited'): 
     if filename.is_file():
         im2=cv2.imread(filename.path)
         image_method.append(im2)
         names_method.append(filename.path)
-
 count=0
+
 while(count<48):
     count_original = math.floor(count/4)
-
     if (count/4 == math.floor(count/4)):
         b1_rgb, g1, r1 = cv2.split(image_original[count_original])
         yuv_img1 = cv2.cvtColor(image_original[count_original], cv2.COLOR_BGR2YUV)
         y1, u1, v1 = cv2.split(yuv_img1) 
         lab_img1 = cv2.cvtColor(image_original[count_original], cv2.COLOR_BGR2LAB)
         l1, a1, b1_lab = cv2.split(lab_img1) 
-
     b2_rgb, g2, r2 = cv2.split(image_method[count])
     yuv_img2 = cv2.cvtColor(image_method[count], cv2.COLOR_BGR2YUV)
     y2, u2, v2 = cv2.split(yuv_img2)
     lab_img2 = cv2.cvtColor(image_method[count], cv2.COLOR_BGR2LAB)
     l2, a2, b2_lab = cv2.split(lab_img2) 
-
     img_name = names_method[count].split('\\')[1][:-4]
     print(img_name)
-
     file_psnr.write(img_name)
     file_psnr.write('/')
     file_ssim.write(img_name)
@@ -59,15 +51,12 @@ while(count<48):
     psnr_g = sewar.full_ref.psnr(g1, g2, MAX=None)
     psnr_b_rgb = sewar.full_ref.psnr(b1_rgb, b2_rgb, MAX=None)
     psnr_rgb = (psnr_r + psnr_g + psnr_b_rgb)/3
-
     psnr_u = sewar.full_ref.psnr(u1, u2, MAX=None)
     psnr_v = sewar.full_ref.psnr(v1, v2, MAX=None)
     psnr_uv = (psnr_u + psnr_v)/2
-
     psnr_a = sewar.full_ref.psnr(a1, a2, MAX=None)
     psnr_b_lab = sewar.full_ref.psnr(b1_lab, b2_lab, MAX=None)
     psnr_ab = (psnr_a + psnr_b_lab)/2
-
     data = (str(psnr_r) + ':' + str(psnr_g) + ':' + str(psnr_b_rgb) + ':' + str(psnr_rgb) + '|' + 
         str(psnr_u) + ':' + str(psnr_v) + ':' + str(psnr_uv) + '|' + 
         str(psnr_a) + ':' + str(psnr_b_lab) + ':' + str(psnr_ab) + '\n' )
@@ -78,15 +67,12 @@ while(count<48):
     ssim_g, cs_g = sewar.full_ref.ssim(g1, g2, ws=11, MAX=None, fltr_specs=None, mode='valid')
     ssim_b_rgb, cs_b_rgb = sewar.full_ref.ssim(b1_rgb, b2_rgb, ws=11, MAX=None, fltr_specs=None, mode='valid')
     ssim_rgb = (ssim_r + ssim_g + ssim_b_rgb)/3
-
     ssim_u, cs_u = sewar.full_ref.ssim(u1, u2, ws=11, MAX=None, fltr_specs=None, mode='valid')
     ssim_v, cs_v = sewar.full_ref.ssim(v1, v2, ws=11, MAX=None, fltr_specs=None, mode='valid')
     ssim_uv = (ssim_u + ssim_v)/2
-
     ssim_a, cs_a = sewar.full_ref.ssim(a1, a2, ws=11, MAX=None, fltr_specs=None, mode='valid')
     ssim_b_lab, cs_b_lab = sewar.full_ref.ssim(b1_lab, b2_lab, ws=11, MAX=None, fltr_specs=None, mode='valid')
     ssim_ab = (ssim_a + ssim_b_lab)/2
-
     data = (str(ssim_r) + ':' + str(ssim_g) + ':' + str(ssim_b_rgb) + ':' + str(ssim_rgb) + '|' + 
         str(ssim_u) + ':' + str(ssim_v) + ':' + str(ssim_uv) + '|' + 
         str(ssim_a) + ':' + str(ssim_b_lab) + ':' + str(ssim_ab) + '\n' )
@@ -97,20 +83,16 @@ while(count<48):
     mse_g = sewar.full_ref.mse(g1, g2)
     mse_b_rgb = sewar.full_ref.mse(b1_rgb, b2_rgb)
     mse_rgb = (mse_r + mse_g + mse_b_rgb)/3
-
     mse_u = sewar.full_ref.mse(u1, u2)
     mse_v = sewar.full_ref.mse(v1, v2)
     mse_uv = (mse_u + mse_v)/2
-
     mse_a = sewar.full_ref.mse(a1, a2)
     mse_b_lab = sewar.full_ref.mse(b1_lab, b2_lab)
-    mse_ab = (mse_a + mse_b_lab)/2
-	
+    mse_ab = (mse_a + mse_b_lab)/2	
     data = (str(mse_r) + ':' + str(mse_g) + ':' + str(mse_b_rgb) + ':' + str(mse_rgb) + '|' + 
         str(mse_u) + ':' + str(mse_v) + ':' + str(mse_uv) + '|' + 
         str(mse_a) + ':' + str(mse_b_lab) + ':' + str(mse_ab) + '\n' )
     file_mse.write(data)
-
     count = count+1
     cv2.waitKey(0)
 
